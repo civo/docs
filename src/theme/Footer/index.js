559cd4d782e0e866b72a05ad249bd6f4d6b46314
Tahsin Hasan <tahsin352@yahoo.com>
2025-09-30 00:13:47 +0600

    Rebranding docs

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -7,15 +7,23 @@ export default function FooterWrapper(props) {
       <section class="theme-layout-footer-header footer">
         <div class="container container-fluid">
           <div class="theme-layout-footer-container">
-            <a href="https://www.civo.com/" target="_blank" class=""><div class="navbar__logo"><img src="/docusaurus/img/logo-black.svg" alt="Civo Logo" class="themedComponent_mlkZ themedComponent--light_NVdE" /></div></a>
+            <a href="https://www.civo.com/" target="_blank" class="logo-light"><div class="navbar__logo"><img src="/docusaurus/img/logo-black.svg" alt="Civo Logo" class="themedComponent_mlkZ themedComponent--light_NVdE" /></div></a>
+            <a href="https://www.civo.com/" target="_blank" class="logo-dark"><div class="navbar__logo"><img src="/docusaurus/img/logo-green.svg" alt="Civo Logo" class="" /></div></a>
             <div class="theme-layout-footer-social">
-              <a href="https://www.instagram.com/civocloud/" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/Instagram.svg" alt="Instagram" class="" /></div></a>
-              <a href="https://www.facebook.com/civocloud/" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/Facebook.svg" alt="Civo Logo" class="" /></div></a>
-              <a href="https://x.com/civocloud" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/x-com.svg" alt="Civo Logo" class="" /></div></a>
-              <a href="https://www.linkedin.com/company/civocloud" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/LinkedIn.svg" alt="LinkedIn" class="" /></div></a>
-              <a href="http://www.youtube.com/channel/UCZD2ggK3cnVD_sLMR6gUx2w?sub_confirmation=1&feature=subscribe-embed-click" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/YouTube.svg" alt="Civo Logo" class="" /></div></a>
-              <a href="https://github.com/civo" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/github.svg" alt="Civo Logo" class="" /></div></a>
-              <a href="https://bsky.app/profile/civo.com" target="_blank" class=""><div class="footer-circle"><img src="/docusaurus/img/bluesky.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://www.instagram.com/civocloud/" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/Instagram.svg" alt="Instagram" class="" /></div></a>
+              <a href="https://www.facebook.com/civocloud/" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/Facebook.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://x.com/civocloud" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/x-com.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://www.linkedin.com/company/civocloud" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/LinkedIn.svg" alt="LinkedIn" class="" /></div></a>
+              <a href="http://www.youtube.com/channel/UCZD2ggK3cnVD_sLMR6gUx2w?sub_confirmation=1&feature=subscribe-embed-click" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/YouTube.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://github.com/civo" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/github.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://bsky.app/profile/civo.com" target="_blank" class="light-social"><div class="footer-circle"><img src="/docusaurus/img/bluesky.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://www.instagram.com/civocloud/" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/Instagram-dark.svg" alt="Instagram" class="" /></div></a>
+              <a href="https://www.facebook.com/civocloud/" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/Facebook-dark.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://x.com/civocloud" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/x-com-dark.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://www.linkedin.com/company/civocloud" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/LinkedIn-dark.svg" alt="LinkedIn" class="" /></div></a>
+              <a href="http://www.youtube.com/channel/UCZD2ggK3cnVD_sLMR6gUx2w?sub_confirmation=1&feature=subscribe-embed-click" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/YouTube-dark.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://github.com/civo" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/github-dark.svg" alt="Civo Logo" class="" /></div></a>
+              <a href="https://bsky.app/profile/civo.com" target="_blank" class="dark-social"><div class="footer-circle-dark"><img src="/docusaurus/img/bluesky-dark.svg" alt="Civo Logo" class="" /></div></a>
             </div>
           </div>
           <hr class="footer-hr-color" />
